9551c6b7d397459b8c590567987d8de688819a7a
sachin <sachindhanraj.choudhary@aurionpro.com>
2025-05-31 14:13:34 +0530

    first commit

---
+++ src/main/java/com/demo/cloud_gatway/CloudGatwayApplication.java
@@ -0,0 +1,16 @@
+package com.demo.cloud_gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudGatwayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CloudGatwayApplication.class, args);
+	}
+
+}
